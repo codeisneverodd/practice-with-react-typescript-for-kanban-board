5f4767c515990839d2a6d85ac4ce4985331fa182
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-11 12:08:07 +0900

    Add RecoilRoot to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import {RecoilRoot} from "recoil";
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <RecoilRoot>
+            <App/>
+        </RecoilRoot>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
