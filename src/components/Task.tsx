5496ee0ded3109a79a934de7135d619f025e6a2e
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-11 21:58:28 +0900

    Add style to Wrapper and Add Text.

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,8 +2,16 @@ import {Draggable} from "react-beautiful-dnd";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  width: 100px;
-  background-color: coral;
+  width: 274px;
+  padding: 10px 22px 10px 22px;
+  background-color: ${props => props.theme.taskColor};
+  border-radius: 5px;
+  align-items: center;
+`
+const Text = styled.span`
+  font-size: 14px;
+  line-height: 2;
+  text-align: left;
 `
 
 function Task({id, index, text}: { id: number, index: number, text: string }) {
@@ -11,7 +19,7 @@ function Task({id, index, text}: { id: number, index: number, text: string }) {
         <Draggable draggableId={id + ""} index={index}>
             {(provided) => (
                 <Wrapper ref={provided.innerRef}{...provided.draggableProps}{...provided.dragHandleProps}>
-                    {text}
+                    <Text>{text}</Text>
                 </Wrapper>
             )}
         </Draggable>
