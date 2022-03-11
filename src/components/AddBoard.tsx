d853aafbe3f1364e9c0580d923e2811a637db007
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-11 21:55:56 +0900

    Add style to Input.

--- src/components/AddBoard.tsx
+++ src/components/AddBoard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import {useForm} from "react-hook-form";
-import {useRecoilState} from "recoil";
-import {taskState} from "../models/atoms";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {boardColorSelector, taskState} from "../models/atoms";
 
 const Form = styled.form`
   width: 100%
@@ -16,21 +16,44 @@ const Wrapper = styled.div`
   width: 400px;
   margin: 40px;
 `
+const Input = styled.input<{ boardColor: string }>`
+  width: 122px;
+  margin-left: 10px;
+  font-size: 13px;
+  text-align: center;
+  line-height: 2;
+  color: ${props => props.theme.textColor};
+  padding: 2px 4px;
+  border-radius: 5px;
+  background-color: ${props => props.boardColor}
+}
+
+;
+
+&:focus {
+  outline: none
+}
+
+&::placeholder {
+  color: inherit
+}
+`
 
 function AddBoard() {
     const [tasks, setTasks] = useRecoilState(taskState)
+    const boardColor = useRecoilValue(boardColorSelector)
     const {register, handleSubmit, setValue} = useForm<IForm>()
     const onValid = ({boardId}: IForm) => {
         setTasks(allTasks => ({
             ...allTasks,
-            [boardId + Date.now()]: []
+            [boardId + "+" + Date.now()]: []
         }))
         setValue("boardId", "")
     }
     return (
         <Wrapper>
             <Form onSubmit={handleSubmit(onValid)}>
-                <input {...register("boardId", {required: true})}
+                <Input boardColor={boardColor} autoComplete={"off"} {...register("boardId", {required: true})}
                        placeholder={"+ New Board"}/>
             </Form>
         </Wrapper>
