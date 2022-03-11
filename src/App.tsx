1f0833a054bb2a25b6bfc7d7c702a30e44b470bf
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-11 21:56:33 +0900

    Add ThemeProvider, GlobalStyle, Helmet with metadata.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,92 @@
 import React from 'react';
 import Router from "./Router";
+import {createGlobalStyle, ThemeProvider} from "styled-components";
+import {darkTheme} from "./styles/theme";
+import {Helmet, HelmetProvider} from "react-helmet-async"
 
 function App() {
     return (
-        <>
+        <ThemeProvider theme={darkTheme}>
+            <GlobalStyle/>
+            <HelmetProvider>
+                <Helmet>
+                    <title>Kanban Board</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&family=Pacifico&display=swap"
+                        rel="stylesheet"/>
+                    <meta property="og:image"
+                          content="https://user-images.githubusercontent.com/54318460/157589231-f21fe1b0-c57f-4ef0-8c4e-0a108529d067.png"/>
+                    <meta property="og:description"
+                          content="🔭 Explore 30,000+ cryptos with their real-time price, chart, supplied amount, and more!!"/>
+                </Helmet>
+            </HelmetProvider>
             <Router/>
-        </>
+        </ThemeProvider>
     );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  /* http://meyerweb.com/eric/tools/css/reset/ 
+     v2.0 | 20110126
+     License: none (public domain)
+  */
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+
+  body {
+    line-height: 1;
+    background-color: ${props => props.theme.bgColor};
+    color: ${props => props.theme.textColor};
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  blockquote, q {
+    quotes: none;
+  }
+
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+`
