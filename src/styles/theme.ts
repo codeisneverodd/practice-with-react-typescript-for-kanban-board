1e3c777b1c6010e7021ef30d89390775bc11c515
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-12 11:39:41 +0900

    Add taskDraggingColor, newTaskColor: "#49494950", boardDraggingOverColor

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,5 +4,8 @@ export const darkTheme: DefaultTheme = {
     textColor: "#FFFFFF",
     bgColor: "#383838",
     taskColor: "#212121",
-    trashCanColor: "#E74C3C20"
+    taskDraggingColor: "#494949",
+    newTaskColor: "#49494950",
+    trashCanColor: "#E74C3C20",
+    boardDraggingOverColor: "#424242",
 }
