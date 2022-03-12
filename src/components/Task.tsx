e2c74fff83dfcc1e35f5a07e4dede5fc312feb58
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-12 21:35:45 +0900

    Rename draggingState, setDragging to draggingAtomState, setDraggingAtomState. Rename draggingCard, setDraggingCard to isDragging, setIsDragging

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,45 +1,59 @@
-import {Draggable} from "react-beautiful-dnd";
+import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
-import React, {useEffect, useState} from "react";
-import {useSetRecoilState} from "recoil";
-import {draggingState} from "../models/atoms";
+import React, { useEffect, useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { draggingAtomState } from "../models/atoms";
+
+interface IIndexedTask {
+  id: number;
+  index: number;
+  text: string;
+}
 
 const Wrapper = styled.div<{ isDragging: boolean }>`
   width: 274px;
   padding: 10px 22px 10px 22px;
-  background-color: ${props => props.isDragging ? props => props.theme.taskDraggingColor : props => props.theme.taskColor};
+  background-color: ${(props) =>
+    props.isDragging
+      ? (props) => props.theme.taskDraggingColor
+      : (props) => props.theme.taskColor};
   border-radius: 5px;
   align-items: center;
 
   &:hover {
-    background-color: ${props => props.theme.taskDraggingColor};
+    background-color: ${(props) => props.theme.taskDraggingColor};
   }
-`
+`;
 const Text = styled.span`
   font-size: 14px;
   line-height: 2;
   text-align: left;
-`
+`;
 
-function Task({id, index, text}: { id: number, index: number, text: string }) {
-    const [draggingCard, setDraggingCard] = useState(false)
-    const setDraggingAtomState = useSetRecoilState(draggingState)
-    useEffect(() => { // prevent unlimited re-rendering
-        setDraggingAtomState(draggingCard)
-    }, [draggingCard])
-    return (
-        <Draggable draggableId={id + ""} index={index}>
-            {(provided, snapshot) => {
-                setDraggingCard(snapshot.isDragging)
-                return (
-                    <Wrapper isDragging={snapshot.isDragging}
-                             ref={provided.innerRef}{...provided.draggableProps}{...provided.dragHandleProps}>
-                        <Text>{text}</Text>
-                    </Wrapper>
-                )
-            }}
-        </Draggable>
-    )
+function Task({ id, index, text }: IIndexedTask) {
+  const [isDragging, setIsDragging] = useState(false);
+  const setDraggingAtomState = useSetRecoilState(draggingAtomState);
+  useEffect(() => {
+    // prevent unlimited re-rendering
+    setDraggingAtomState(isDragging);
+  }, [isDragging]);
+  return (
+    <Draggable draggableId={id + ""} index={index}>
+      {({ innerRef, draggableProps, dragHandleProps }, { isDragging }) => {
+        setIsDragging(isDragging);
+        return (
+          <Wrapper
+            isDragging={isDragging}
+            ref={innerRef}
+            {...draggableProps}
+            {...dragHandleProps}
+          >
+            <Text>{text}</Text>
+          </Wrapper>
+        );
+      }}
+    </Draggable>
+  );
 }
 
-export default React.memo(Task)
+export default React.memo(Task);
