ae1d820758cba66a50689f1923e4648592df04c2
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-12 21:36:11 +0900

    Rename draggingState, to draggingAtomState.

--- src/components/TrashCanTask.tsx
+++ src/components/TrashCanTask.tsx
@@ -1,48 +1,51 @@
 import styled from "styled-components";
-import {Droppable} from "react-beautiful-dnd";
+import { Droppable } from "react-beautiful-dnd";
 import React from "react";
-import {useRecoilValue} from "recoil";
-import {draggingState} from "../models/atoms";
+import { useRecoilValue } from "recoil";
+import { draggingAtomState } from "../models/atoms";
 
-const Wrapper = styled.div<{ startDragging: boolean, width: number }>`
+const Wrapper = styled.div<{ startDragging: boolean; width: number }>`
   display: flex;
-  width: ${props => props.width - 400 + "px"};
+  width: ${(props) => props.width - 400 + "px"};
   height: 200px;
   flex-direction: column;
   margin: auto;
   border-radius: 0 0 20px 20px;
-  background-color: ${props => props.startDragging ? props => props.theme.trashCanColor : 'none'};
-`
+  background-color: ${(props) =>
+    props.startDragging ? (props) => props.theme.trashCanColor : "none"};
+`;
 const Area = styled.div<{ isDraggingOver: boolean }>`
   width: 100%;
   border-radius: 0 0 20px 20px;
-  background-color: ${props => props.isDraggingOver ? props => props.theme.trashCanColor : 'none'};
+  background-color: ${(props) =>
+    props.isDraggingOver ? (props) => props.theme.trashCanColor : "none"};
   flex-grow: 1;
-`
+`;
 const Text = styled.div<{ isDraggingOver: boolean }>`
-  display: ${props => props.isDraggingOver ? 'block' : 'none'};
+  display: ${(props) => (props.isDraggingOver ? "block" : "none")};
   font-size: 20px;
   text-align: center;
   line-height: 2;
-`
+`;
 
-
-function TrashCanTask({width}: { width: number }) {
-    const dragging = useRecoilValue(draggingState)
-    console.log(width)
-    return (
-        <Wrapper startDragging={dragging} width={width}>
-            <Droppable droppableId={"trashCanCard"}>
-                {(provided, snapshot) => (
-                    <Area isDraggingOver={snapshot.isDraggingOver}
-                          ref={provided.innerRef} {...provided.droppableProps}>
-                        {provided.placeholder}
-                        <Text isDraggingOver={snapshot.isDraggingOver}>REMOVE TASK</Text>
-                    </Area>
-                )}
-            </Droppable>
-        </Wrapper>
-    )
+function TrashCanTask({ width }: { width: number }) {
+  const dragging = useRecoilValue(draggingAtomState);
+  return (
+    <Wrapper startDragging={dragging} width={width}>
+      <Droppable droppableId={"trashCanCard"}>
+        {(provided, snapshot) => (
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {provided.placeholder}
+            <Text isDraggingOver={snapshot.isDraggingOver}>REMOVE TASK</Text>
+          </Area>
+        )}
+      </Droppable>
+    </Wrapper>
+  );
 }
 
-export default React.memo(TrashCanTask)
+export default React.memo(TrashCanTask);
