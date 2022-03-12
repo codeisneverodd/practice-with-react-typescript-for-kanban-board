52d887eef93111db1d5f0e13054ee3792a027917
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-12 11:38:50 +0900

    Add dragging logic and Improve background-color to changing value.

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,12 +1,19 @@
 import {Draggable} from "react-beautiful-dnd";
 import styled from "styled-components";
+import React, {useEffect, useState} from "react";
+import {useSetRecoilState} from "recoil";
+import {draggingState} from "../models/atoms";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ isDragging: boolean }>`
   width: 274px;
   padding: 10px 22px 10px 22px;
-  background-color: ${props => props.theme.taskColor};
+  background-color: ${props => props.isDragging ? props => props.theme.taskDraggingColor : props => props.theme.taskColor};
   border-radius: 5px;
   align-items: center;
+
+  &:hover {
+    background-color: ${props => props.theme.taskDraggingColor};
+  }
 `
 const Text = styled.span`
   font-size: 14px;
@@ -15,15 +22,24 @@ const Text = styled.span`
 `
 
 function Task({id, index, text}: { id: number, index: number, text: string }) {
+    const [draggingCard, setDraggingCard] = useState(false)
+    const setDraggingAtomState = useSetRecoilState(draggingState)
+    useEffect(() => { // prevent unlimited re-rendering
+        setDraggingAtomState(draggingCard)
+    }, [draggingCard])
     return (
         <Draggable draggableId={id + ""} index={index}>
-            {(provided) => (
-                <Wrapper ref={provided.innerRef}{...provided.draggableProps}{...provided.dragHandleProps}>
-                    <Text>{text}</Text>
-                </Wrapper>
-            )}
+            {(provided, snapshot) => {
+                setDraggingCard(snapshot.isDragging)
+                return (
+                    <Wrapper isDragging={snapshot.isDragging}
+                             ref={provided.innerRef}{...provided.draggableProps}{...provided.dragHandleProps}>
+                        <Text>{text}</Text>
+                    </Wrapper>
+                )
+            }}
         </Draggable>
     )
 }
 
-export default Task
+export default React.memo(Task)
