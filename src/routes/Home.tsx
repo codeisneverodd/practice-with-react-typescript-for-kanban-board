a24f09c7169915f4738d119d4ff3ee4fc3ccadef
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-12 11:58:23 +0900

    Refactor TrashCan, split TrashCan into TrashCanBoard.tsx, TrashCanTask.tsx

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,17 +3,17 @@ import {DragDropContext, Droppable, DropResult} from "react-beautiful-dnd";
 import styled from "styled-components";
 import {useRecoilState, useSetRecoilState} from "recoil";
 import {draggingState, taskState} from "../models/atoms";
-import TrashCan from "../components/TrashCan";
+import TrashCan from "../components/TrashCanTask";
 import AddBoard from "../components/AddBoard";
 import {useEffect} from "react";
 import {saveLocal} from "../models/localStorage";
+import TrashCanBoard from "../components/TrashCanBoard";
 
 const Boards = styled.div`
   display: flex;
   flex-grow: 1;
   overflow-x: auto;
   align-items: baseline;
-  padding-left: 200px;
 
   &::-webkit-scrollbar {
     display: none;
@@ -21,7 +21,7 @@ const Boards = styled.div`
 `
 const Grid = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
 `
 
 function Home() {
@@ -96,6 +96,7 @@ function Home() {
                 <Droppable droppableId={"boards"} type={"boards"} direction={"horizontal"}>
                     {(provided) => (
                         <Grid ref={provided.innerRef} {...provided.droppableProps}>
+                            <TrashCanBoard/>
                             <Boards>
                                 {Object.keys(tasks).map((boardId, index) =>
                                     <Board key={boardId} index={index} boardId={boardId}
