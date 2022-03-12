28eebcf07433b707fbb319e1bdb162e823b183a6
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-12 21:02:08 +0900

    Simplify Board.tsx

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,31 @@
-import {Draggable, Droppable} from "react-beautiful-dnd";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 import Task from "./Task";
 import styled from "styled-components";
-import {ITask, taskState} from "../models/atoms";
-import {useForm} from "react-hook-form";
-import {useSetRecoilState} from "recoil";
-import React, {useRef} from "react";
-import binImg from "../images/bin.png"
+import { ITask, taskState } from "../models/atoms";
+import { useForm } from "react-hook-form";
+import { useSetRecoilState } from "recoil";
+import React from "react";
+import binImg from "../images/bin.png";
+
+interface IForm {
+  task: string;
+}
+
+interface IBoard {
+  boardId: string;
+  tasks: ITask[];
+  index: number;
+}
 
 const Wrapper = styled.div`
+  // Wrapping Handle, Area
   display: flex;
   flex-direction: column;
   width: 322px;
   padding-left: 200px;
-
-`
-const Form = styled.form`
-  width: 100%
-`
-
-interface IForm {
-    task: string;
-}
-
-const Area = styled.div<{ isDraggingOver: boolean }>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  row-gap: 2px;
-  flex-grow: 1;
-  min-height: 400px;
-  background-color: ${props => props.isDraggingOver ? props.theme.boardDraggingOverColor : "none"};
-`
+`;
 const Handle = styled.div`
+  // Handle of board when dragging
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -45,13 +39,19 @@ const Handle = styled.div`
   }
 
   &:hover {
-
     img {
       display: block;
     }
   }
-`
+`;
+const Header = styled.div`
+  // Head of Board
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
 const Label = styled.div<{ boardColor: string }>`
+  // Showing board Name
   width: 122px;
   margin-left: 10px;
   font-size: 13px;
@@ -59,103 +59,130 @@ const Label = styled.div<{ boardColor: string }>`
   line-height: 2;
   padding: 2px 4px;
   border-radius: 5px;
-  background-color: ${props => props.boardColor ?? "#333333"};
-`
+  background-color: ${(props) => props.boardColor ?? "#333333"};
+`;
+const TaskCount = styled.div`
+  // Showing how many tasks in the board.
+  padding-left: 10px;
+  color: ${(props) => props.theme.textBlurColor};
+`;
+const DeleteImgButton = styled.img``;
+const Form = styled.form`
+  width: 100%;
+`;
 const Input = styled.input`
   width: 274px;
   padding: 10px 22px 10px 22px;
+  //styling
   border-style: solid;
-  border-color: #FFFFFF00;
+  border-radius: 5px;
+  border-color: transparent;
+  outline: none;
   background-color: inherit;
+  //font
   font-size: 14px;
   line-height: 2;
   text-align: left;
-  color: ${props => props.theme.textColor};
-  outline: none;
+  color: ${(props) => props.theme.textColor};
 
-  &:hover, &:focus {
+  &:hover,
+  &:focus {
     outline: none;
-    border-radius: 5px;
-    background-color: ${props => props.theme.newTaskColor};
+    background-color: ${(props) => props.theme.newTaskColor};
   }
 
   &::placeholder {
-    color: inherit
+    color: ${(props) => props.theme.textBlurColor};
   }
-`
-const Header = styled.div`
+`;
+const Area = styled.div<{ isDraggingOver: boolean }>`
   display: flex;
-  flex-direction: row;
+  min-height: 400px;
+  flex-direction: column;
+  flex-grow: 1;
   align-items: center;
-`
-const LengthText = styled.div`
-  padding-left: 10px;
-  color: ${props => props.theme.textBlurColor}
-`
+  row-gap: 2px;
+  background-color: ${(props) =>
+    props.isDraggingOver ? props.theme.boardDraggingOverColor : "none"};
+`;
 
-interface IBoard {
-    boardId: string,
-    tasks: ITask[],
-    index: number
-}
-
-function Board({boardId, tasks, index}: IBoard) {
-    const inputRef = useRef<HTMLFormElement>(null)
-    const setTasks = useSetRecoilState(taskState)
-    const {register, handleSubmit, setValue} = useForm<IForm>()
-    const onValid = ({task}: IForm) => {
-        const newTask = {id: Date.now(), text: task}
-        setTasks(allTasks => ({
-            ...allTasks,
-            [boardId]: [newTask, ...allTasks[boardId]],
-        }))
-        console.log(inputRef)
-        inputRef?.current?.focus()
-        setValue("task", "")
+function Board({ boardId, tasks, index }: IBoard) {
+  const boardColorInId = boardId.slice(
+    boardId.length - 21,
+    boardId.length - 14
+  );
+  const boardNameInId = boardId.slice(0, boardId.length - 22);
+  const setTasks = useSetRecoilState(taskState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const onValid = ({ task }: IForm) => {
+    const newTask = { id: Date.now(), text: task }; // unique ID by Date.now()
+    setTasks((allTasks) => ({
+      ...allTasks,
+      [boardId]: [newTask, ...allTasks[boardId]],
+    }));
+    setValue("task", "");
+  };
+  const onClick = () => {
+    // Remove board
+    if (
+      window.confirm(
+        `️Are you sure to remove "${boardId.slice(0, boardId.length - 22)}" ⁉`
+      )
+    ) {
+      setTasks((allTasks) => {
+        let boardEntries = Object.entries(allTasks);
+        boardEntries.splice(index, 1);
+        return boardEntries.reduce(
+          (modifiedBoards, [boardId, tasks]) => ({
+            ...modifiedBoards,
+            [boardId]: tasks,
+          }),
+          {}
+        );
+      });
     }
-    const onClick = () => {
-        if (window.confirm(`Are you sure deleting "${boardId.slice(0, boardId.length - 22)}"? `)) {
-            setTasks(allTasks => {
-                let entries = Object.entries(allTasks)
-                entries.splice(index, 1)
-                return entries.reduce((r, [k, v]) => ({...r, [k]: v}), {})
-            })
-        }
-    }
-    return (
-        <Draggable draggableId={boardId} index={index}>
-            {(provided) => (
-                <Wrapper ref={provided.innerRef} {...provided.draggableProps}>
-                    <Handle {...provided.dragHandleProps}>
-                        <Header>
-                            <Label
-                                boardColor={boardId.slice(boardId.length - 21, boardId.length - 14)}>{boardId.slice(0, boardId.length - 22)}
-                            </Label>
-                            <LengthText>{tasks.length}</LengthText>
-                        </Header>
-                        <img onClick={onClick} alt={"binImg"} src={binImg}/>
-                    </Handle>
-                    <Droppable droppableId={boardId}>
-                        {(provided, snapshot) => (
-                            <Area ref={provided.innerRef} {...provided.droppableProps}
-                                  isDraggingOver={snapshot.isDraggingOver}>
-                                {tasks.map((task, index) => <Task key={task.id} id={task.id} index={index}
-                                                                  text={task.text}/>)}
-                                {provided.placeholder}
-                                <Form onSubmit={handleSubmit(onValid)} ref={inputRef}>
-                                    <Input
-                                        autoComplete={"off"} {...register("task", {required: true})}
-                                        placeholder={"+ New"}/>
-                                </Form>
-                            </Area>
-                        )}
-                    </Droppable>
-                </Wrapper>
+  };
+  return (
+    <Draggable draggableId={boardId} index={index}>
+      {(provided) => (
+        <Wrapper ref={provided.innerRef} {...provided.draggableProps}>
+          <Handle {...provided.dragHandleProps}>
+            <Header>
+              <Label boardColor={boardColorInId}>{boardNameInId}</Label>
+              <TaskCount>{tasks.length}</TaskCount>
+            </Header>
+            <DeleteImgButton onClick={onClick} alt={"binImg"} src={binImg} />
+          </Handle>
+          <Droppable droppableId={boardId}>
+            {(provided, snapshot) => (
+              <Area
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                isDraggingOver={snapshot.isDraggingOver}
+              >
+                {tasks.map((task, index) => (
+                  <Task
+                    key={task.id}
+                    id={task.id}
+                    index={index}
+                    text={task.text}
+                  />
+                ))}
+                {provided.placeholder}
+                <Form onSubmit={handleSubmit(onValid)}>
+                  <Input
+                    autoComplete={"off"}
+                    {...register("task", { required: true })}
+                    placeholder={"+ New"}
+                  />
+                </Form>
+              </Area>
             )}
-        </Draggable>
-
-
-    )
+          </Droppable>
+        </Wrapper>
+      )}
+    </Draggable>
+  );
 }
 
-export default React.memo(Board)
+export default React.memo(Board);
