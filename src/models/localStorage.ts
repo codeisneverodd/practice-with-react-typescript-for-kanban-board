50ced72640881acfd637da274d99ef63fcf18a1a
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-12 20:31:15 +0900

    Simplify localStorage.ts and Rename saveLocal to saveTasks.

--- src/models/localStorage.ts
+++ src/models/localStorage.ts
@@ -1,29 +1,28 @@
-import {ITaskState} from './atoms';
+import { ITaskState } from "./atoms";
 
-export const LOCAL_TASK_STATE = 'recoil_tasks';
-export const LOCAL_COLOR_COUNT_STATE = 'recoil_color_count';
+export const LOCAL_TASK_STATE = "recoil_tasks";
+export const LOCAL_COLOR_COUNT_STATE = "recoil_color_count";
 
 export const loadTasks = () => {
-    const localTasks = localStorage.getItem(LOCAL_TASK_STATE);
-    if (localTasks) {
-        return JSON.parse(localTasks);
-    }
-    return null;
+  const localTasks = localStorage.getItem(LOCAL_TASK_STATE);
+  if (localTasks) {
+    return JSON.parse(localTasks);
+  }
+  return null;
 };
 
 export const loadColorCount = () => {
-    const localTasks = localStorage.getItem(LOCAL_COLOR_COUNT_STATE);
-    if (localTasks) {
-        const json = JSON.parse(localTasks)
-        return json.count + 1
-    }
-    return 3;
+  const localTasks = localStorage.getItem(LOCAL_COLOR_COUNT_STATE);
+  if (localTasks) {
+    const json = JSON.parse(localTasks);
+    return json.count + 1;
+  }
+  return null;
 };
 
-export const saveLocal = (tasks: ITaskState) => {
-    localStorage.setItem(LOCAL_TASK_STATE, JSON.stringify(tasks));
-
+export const saveTasks = (tasks: ITaskState) => {
+  localStorage.setItem(LOCAL_TASK_STATE, JSON.stringify(tasks));
 };
 export const saveColorCount = (colorCount: object) => {
-    localStorage.setItem(LOCAL_COLOR_COUNT_STATE, JSON.stringify(colorCount));
+  localStorage.setItem(LOCAL_COLOR_COUNT_STATE, JSON.stringify(colorCount));
 };
