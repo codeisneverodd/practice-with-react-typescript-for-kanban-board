32c8ebe677e1fdf1b71d506b6870c66f09416de4
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-13 09:40:44 +0900

    Add limit on number of boards

--- src/components/AddBoard.tsx
+++ src/components/AddBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import { useRecoilValue, useSetRecoilState } from "recoil";
@@ -17,18 +17,25 @@ const Wrapper = styled.div`
 const NewBoardForm = styled.form`
   width: 100%;
 `;
-const NewBoardInput = styled.input<{ boardColor: string }>`
+const NewBoardInput = styled.input<{ boardColor: string; disabled: boolean }>`
   width: 122px;
   padding: 2px 4px;
   margin-left: 10px;
   //style
+  border-style: ${(props) => (props.disabled ? "none" : "inherit")};
   border-radius: 5px;
-  background-color: ${(props) => props.boardColor};
+  background-color: ${(props) =>
+    props.disabled
+      ? (props) => props.theme.bgColor
+      : (props) => props.boardColor};
   //font
   font-size: 13px;
   text-align: center;
   line-height: 2;
-  color: ${(props) => props.theme.textColor};
+  color: ${(props) =>
+    props.disabled
+      ? (props) => props.theme.textBlurColor
+      : (props) => props.theme.textColor};
 
   &:focus {
     outline: none;
@@ -45,11 +52,23 @@ const NewBoardInput = styled.input<{ boardColor: string }>`
 const ErrorMessage = styled.div`
   padding: 30px 10px 10px 10px;
   text-align: left;
+  color: ${(props) => props.theme.textBlurColor};
+`;
+
+const LimitMessage = styled.div<{ disabled: boolean }>`
+  display: ${(props) => (props.disabled ? "block" : "none")};
+  min-height: 400px;
+  color: transparent;
+
+  &:hover {
+    color: ${(props) => props.theme.textBlurColor};
+  }
 `;
 
 function AddBoard({ index }: { index: number }) {
   const setTasks = useSetRecoilState(taskState);
   const colors = useRecoilValue(boardColorList);
+  const [disabled, setDisabled] = useState(false);
   const [colorCount, setColorCount] = useState(loadColorCount);
   const nextColor = colors[colorCount % colors.length];
   const {
@@ -69,12 +88,20 @@ function AddBoard({ index }: { index: number }) {
     saveColorCount({ count: colorCount });
     setValue("boardId", "");
   };
+  useEffect(() => {
+    console.log(index);
+    index > 4 ? setDisabled(true) : setDisabled(false);
+  }, [index]);
   return (
     <Draggable draggableId={"addBoard"} index={index}>
       {({ innerRef, draggableProps, dragHandleProps }) => (
         <Wrapper ref={innerRef} {...draggableProps} {...dragHandleProps}>
+          <LimitMessage disabled={disabled}>
+            👻 LIMITED TO 5 BOARDS 👻
+          </LimitMessage>
           <NewBoardForm onSubmit={handleSubmit(onValid)}>
             <NewBoardInput
+              disabled={disabled}
               boardColor={nextColor}
               autoComplete={"off"}
               {...register("boardId", {
@@ -84,7 +111,7 @@ function AddBoard({ index }: { index: number }) {
                   message: "Board name must be less than 12 characters",
                 },
               })}
-              placeholder={"+ New Board"}
+              placeholder={disabled ? "" : "+ New Board"}
             />
             <ErrorMessage>{errors?.boardId?.message}</ErrorMessage>
           </NewBoardForm>
